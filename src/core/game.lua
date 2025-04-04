7b7a5d628c339ccbbba5c6db225bd3e62873d865
RakkenTi <martin3zmrk@gmail.com>
2025-04-04 06:15:38 -0400

    added new files

---
+++ src/core/game.lua
@@ -0,0 +1,17 @@
+--- Core game lua file.
+--- main.lua is seperate as it is the entry point of the program.
+--- This file should act as the center point of the game.
+
+local game = {}
+
+--- Called by love.init()
+function game.init()
+
+end
+
+--- Called in love.update()
+function game.update(dt)
+
+end
+
+return game
